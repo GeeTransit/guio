a0473778cddef14a1d324ddb7226b293b60742b4
GeeTransit <geetransit@gmail.com>
2019-08-22 16:10:08 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="guio",
+    description="Guio - Curio-Tkinter Compatible Kernel",
+    long_description="Guio is a library offering a curio kernel with tkinter support.",
+    license="MIT",
+    version="0.9",
+    author="George Zhang",
+    author_email="geetransit@example.com",
+    url="https://github.com/GeeTransit/guio",
+    packages=["guio"],
+    install_requires=["curio"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
