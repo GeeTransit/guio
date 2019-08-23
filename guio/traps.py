a6d803728f2c18aebd8d26602903dea35b0bbf4a
GeeTransit <geetransit@gmail.com>
2019-08-23 14:18:49 -0400

    Fix traps

--- guio/traps.py
+++ guio/traps.py
@@ -4,13 +4,13 @@ from curio.traps import _kernel_trap
 __all__ = ["_pop_event", "_wait_event", "_get_toplevel"]
 
 
-def _pop_event():
+async def _pop_event():
     return await _kernel_trap("_trap_pop_event")
 
 
-def _wait_event():
+async def _wait_event():
     return await _kernel_trap("_trap_wait_event")
 
 
-def _get_toplevel():
+async def _get_toplevel():
     return await _kernel_trap("_trap_get_toplevel")
